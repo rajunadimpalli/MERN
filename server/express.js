18d21c5a18f37d79b57c94e9991f758cdc518373
rajunadimpalli <sraju1521@gmail.com>
2018-07-03 15:56:50 -0400

    new changes

---
+++ server/express.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+
+import Template from './../template'
+import userRoutes from './routes/user.routes'
+import authRoutes from './routes/auth.routes'
+
+const app = express()
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+// secure apps by setting various HTTP headers
+app.use(helmet())
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors())
+app.get('/', (req, res) => {
+    res.status(200).send(Template())
+})
+app.use('/', userRoutes)
+app.use('/', authRoutes)
+
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({
+            "error":err.name + ": " + err.message
+        })
+    }
+})
+
+
+//import config from '../config/config'
+
+
+export default app
